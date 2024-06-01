83e23aad598d6f25ceb68ce0efdafab7ef5a6e48
djannak <165226837+djannak@users.noreply.github.com>
2024-06-01 15:41:55 +0100

    create function to get computer's choice
    written function to determine the computer's choice for the game being coded

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 console.log("hello world, i know you weren't expecting to see me here")
+
+function getComputerResult() {
+
+    let Choice = Math.floor(Math.random() * 3);
+    
+        if (Choice === 0) {
+        return "rock";
+        }
+    
+        else if (Choice === 1) {
+        return "paper";
+        }
+    
+        else (Choice === 2) 
+        return "scissors";
+    }
+
+    
